97c65305d2b21b4e8d20e53c8380c9f91aa3b337
daniastu06 <astudillori.danie@student.ccm.edu>
2026-09-18 14:06:22 -0400

    almost completing lab1

--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,21 @@
 public class PersonTest {
-    
+    public static void main(String[] args) {
+        Person person1 = new Person();
+
+        person1.setName("Daniel Astudillo");
+        person1.setAge(20);
+        person1.setmail("danielA@gmail.com");
+        
+        Person person2 = new Person();
+        
+        person2.setName("Felipe Castaneda");
+        person2.setAge(18);
+        person2.setmail("felipeC@gmail.com");
+
+        System.out.println("First Person");
+        person1.displayInfo();
+
+        System.out.println("Second Person");
+        person2.displayInfo();
+    }
 }
